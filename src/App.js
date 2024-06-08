086228712bbf62c7dbc605350fc72c5302db53bb
Khushi1524 <khushitiwari2415@gmail.com>
2024-06-08 21:09:07 +0530

    Started the new react project image search from unsplash api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Imagecard from "./Imagecard";
 
-function App() {
+//  const API_URL = 'https://api.unsplash.com/photos/?client_id=BIuquMALEZk40BV3IOvH04GW6-JkUdfBPkrqjHoYkRo'
+
+const App = () => {
+  const [images, setImages] = useState([]);
+
+  const searchImage = async () => {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/?client_id=BIuquMALEZk40BV3IOvH04GW6-JkUdfBPkrqjHoYkRo`
+    );
+    const data = await response.json();
+
+    setImages(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchImage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <h1>Image Search</h1>
+        
+
+        <div className="search">
+          <input placeholder="Search images here"
+           onChange={() => {}} />
+        </div>
+
+        <div className="button">
+          <button onClick={searchImage} >Search</button>
+        </div>
+          
+        {images?.length > 0 ? (
+        <div className="imagestructure">
+          {images.map((image) => (
+            <Imagecard images={images} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Image found</h2>
+        </div>
+      )}
+       
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Imagecard.jsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react"
+
+const Imagecard = (images) =>{
+    render(
+        <div className="imageStructure">
+          <div>
+          <img src={} alt="uplash image" />
+          </div>
+        </div>
+    )
+}
+
+export default Imagecard;
