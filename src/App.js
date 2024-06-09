48d08eb6fde359355595ce57e42620bd677bc763
Khushi1524 <khushitiwari2415@gmail.com>
2024-06-09 12:50:39 +0530

    completed the structig of the page

--- src/App.js
+++ src/App.js
@@ -4,47 +4,50 @@ import Imagecard from "./Imagecard";
 
 const App = () => {
   const [images, setImages] = useState([]);
-  const [searchTerm, setSearchTerm] = useState([])
+  const [searchTerm, setSearchTerm] = useState([]);
 
   const searchImage = async (query) => {
     const response = await fetch(
-      `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=BIuquMALEZk40BV3IOvH04GW6-JkUdfBPkrqjHoYkRo`
+      `https://api.unsplash.com/search/photos?page=1&query=${query}&per_page=30&page=+&client_id=BIuquMALEZk40BV3IOvH04GW6-JkUdfBPkrqjHoYkRo`
     );
     const data = await response.json();
 
     setImages(data.results);
     console.log(data.results);
-
   };
 
   useEffect(() => {
-    searchImage('bird');
+    searchImage("nature");
   }, []);
 
   return (
     <>
       <div className="app">
-        <h1>Image Search</h1>
-        
+        <h1>SEEKPIX</h1>
 
         <div className="search">
-          <input placeholder="Search images here"
-          value={searchTerm}
-           onChange={(e) => {setSearchTerm(e.target.value)}} />
+          <input
+            placeholder="Search images here"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+
+          <button
+            onClick={() => {
+              searchImage(searchTerm);
+            }}
+          >
+            Search
+          </button>
         </div>
 
-        <div className="button">
-          <button onClick={() =>{searchImage(searchTerm)}} >Search</button>
+        <div className="imgCollection">
+          {images.map((image) => {
+            return <Imagecard image={image} key={image.id} />;
+          })}
         </div>
-          
-     <div className="imgCollection" >
-      {images.map((image)=>{
-          return (
-           <Imagecard image={image} key={image.id} />
-          )
-      })}
-      </div>
-       
       </div>
     </>
   );
--- src/Imagecard.js
+++ src/Imagecard.js
@@ -1,19 +1,15 @@
+const Imagecard = ({ image }) => {
+  return (
+    <div className="card">
+      <img src={image.urls.small} />
 
-const Imagecard = ({image}) =>{
-    return(
-       
-         <div className="card">
-             <img
-            src={image.urls.small}
-            />
-
-            <p>{image.user.username}</p>
-            <p>{image.alt_description}</p>
-         </div>
       
 
-
-    )
-}
+      <p>{image.user.username}</p>
+      {/* <p>{image.alt_description}</p> */}
+      <img src={image.user.profile_image.small} />
+    </div>
+  );
+};
 
 export default Imagecard;
